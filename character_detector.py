549f04adf28053ad9f32d3fd5795850c791c7a13
fstetic <filip.stetic74@gmail.com>
2020-12-05 16:17:24 +0100

    Finished character_detector.py

--- character_detector.py
+++ character_detector.py
@@ -1,4 +1,5 @@
 import cv2
+import numpy as np
 
 def detect(image):
 	"""
@@ -9,18 +10,31 @@ def detect(image):
 	:return cropped image: list of numpy.arrays
 		bounding boxes of detected elements
 	"""
+	# resize image to a fixed dimension
+	img = cv2.resize(image, (500,500))
+
 	# convert the image to grayscale
-	gray_image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+	gray_image = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+
+	# apply threshold to differentiate foreground and background easier
+	thresh = cv2.adaptiveThreshold(gray_image, 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C ,cv2.THRESH_BINARY, 11, 10)
+
+	# invert the colors
+	inverted_image = cv2.bitwise_not(thresh)
+
+	# perform dilatation to increase symbol thickness
+	dilatation = cv2.dilate(inverted_image, np.ones((2,2)), iterations=3)
 
 	# find contours
-	contours, hierarchy = cv2.findContours(gray_image, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+	contours, hierarchy = cv2.findContours(dilatation, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
 
 	coordinates, cropped_images = list(), list()
 	for cnt in contours:
-		x, y, w, h = cv2.boundingRect(cnt)
-		# save coordinates
-		coordinates.append((x,y))
-		# save cropped image
-		cropped_images.append(image[y:y + h, x:x + w])
+		if cv2.contourArea(cnt) > 100:
+			x, y, w, h = cv2.boundingRect(cnt)
+			# save coordinates
+			coordinates.append((x,y))
+			# save cropped image
+			cropped_images.append(255 - img[y:y + h, x:x + w])
 
 	return coordinates, cropped_images
