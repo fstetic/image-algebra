fda88e6f4e88ed7d5f6f0d034d6d313fffd95368
fstetic <filip.stetic74@gmail.com>
2020-12-07 12:22:56 +0100

    Implemented math_solver.py using shunting_yard algorithm

--- math_solver.py
+++ math_solver.py
@@ -12,7 +12,7 @@ def make_equation(coordinates, characters):
 		equation made from characters
 	"""
 	# get x coordinates from tuples
-	x_coordinates = [c[1] for c in coordinates]
+	x_coordinates = [c[0] for c in coordinates]
 
 	# pair them up with characters in a dictionary
 	x_c_pairs = {x:c for x,c in zip(x_coordinates, characters)}
@@ -21,40 +21,122 @@ def make_equation(coordinates, characters):
 	equation = ""
 	for x in sorted(x_c_pairs):
 		equation += x_c_pairs[x] + ' '
-	return equation
+
+	# remove the last space
+	return equation[:-1]
 
 
 def solve(equation):
 	"""
+	Solves equation using shunting-yard algorithm
 	:param equation: string
 		equation to be solved
 	:return: float
 		result of equation
 	"""
-	# solve everything inside of brackets first and put it back in list
-	if '(' in equation:
-		# inside is index of first bracket and index of last bracket
-		index_of_first_bracket = equation.index('(')
-		index_of_last_bracket = len(equation) - 1 - equation[::-1].index(')')
-
-		# replace it with solved expression
-		equation = equation.replace(equation[index_of_first_bracket:index_of_last_bracket+1],
-		                 str(solve(equation[index_of_first_bracket+1:index_of_last_bracket])))
+	postfix = rpn(equation)
+	result = shunting_yard(postfix)
+	return result
 
 
-	# solve equation
-	current_number = 0
-	current_operator = ''
-	for c in equation:
-		if c.isdigit():
-			if not current_operator:
-				current_number += float(c)
+def rpn(equation):
+	"""
+	Takes equation and returns reverse polish notation for it
+	i.e. performs infix to postfix conversion
+	:param equation: string
+		equation to parse
+	:return: list
+		rpn of equation
+	"""
+	precedence = {'+':3, '-':3, '*':2, '/':2, '(':1}    # needed to respect the order of operations
+	operators = ['+','-','*','/']   # needed for error handling
+	operator_stack, postfix_result = list(), list()
+	elements = equation.split(' ')
+
+	# look if expression ends in an operator
+	found_operator = False
+	for e in elements[::-1]:
+		if e.isdigit():
+			# if there's an operator after the last digit
+			if found_operator:
+				raise_error("Unterminated expression", equation)
 			else:
-				if current_operator=='+':
-					current_number += float(c)
-				elif current_operator=='-':
-					current_number -= float(c)
-		else:
-			current_operator = c
+				break
+		elif e in operators:
+			found_operator = True
 
-	return current_number
+	for e in elements:
+		if e.isdigit():
+			# just add digits
+			postfix_result.append(e)
+		elif e == '(':
+			# start the operands in brackets
+			operator_stack.append(e)
+		elif e == ')':
+			top_operand = operator_stack.pop()
+			# while inside the bracket
+			while top_operand != '(':
+				# add all operands
+				postfix_result.append(top_operand)
+				top_operand = operator_stack.pop()
+		else:
+			if e not in operators:
+				raise_error("Unsupported character", equation)
+			# while there are more important operands on stack, add them before current
+			while operator_stack and precedence[operator_stack[-1]] >= precedence[e]:
+				postfix_result.append(operator_stack.pop())
+			operator_stack.append(e)
+
+	# empty the stack
+	while operator_stack:
+		# if there are brackets at this point it means some were not opened or closed
+		if '(' in operator_stack or ')' in operator_stack:
+			raise_error("Missing brackets", equation)
+		postfix_result.append(operator_stack.pop())
+
+	return postfix_result
+
+
+def shunting_yard(elements):
+	"""
+	Performs shunting-yard on a list of characters
+	:param elements: list
+		equation characters in reverse polish notation
+	:return: float
+		result of equation
+	"""
+	# init dict with operations mapping
+	operations = {
+		'+': lambda x, y: x + y,
+		'-': lambda x, y: x - y,
+		'*': lambda x, y: x * y,
+		'/': lambda x, y: x / y,
+	}
+	operand_stack = list()
+
+	for e in elements:
+		if e.isdigit():
+			operand_stack.append(e)
+		else:
+			# take operands from the top of the stack
+			operand2 = float(operand_stack.pop())
+			operand1 = float(operand_stack.pop()) if operand_stack else 0.0 # support for negative numbers
+			# perform operation
+			result = operations[e](operand1, operand2)
+			# return result to the stack
+			operand_stack.append(result)
+
+	# last element on top of the stack is the result
+	return operand_stack.pop()
+
+
+def raise_error(message, equation):
+	"""
+	Raises error and exits execution
+	:param message: string
+		message to display
+	:param equation: string
+		equation in which the error occurred
+	"""
+	print(message,"in equation", equation, file=sys.stderr)
+	exit(1)
